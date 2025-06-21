6ce564980d4c7e203f5188299c52e325966d429d
tulio <tulio89lopes@gmail.com>
2025-06-20 22:41:33 -0300

    feat: adiciona as chamadas das fuções de ordenação

--- main.c
+++ main.c
@@ -5,10 +5,13 @@
 #include "Entities/administrador.h"
 #include "Entities/candidato.h"
 #include "Entities/eleitor.h"
+#include "Ordenacao/classificacaoInterna.h"
 #include "Buscas/buscaSequencial.h"
 #include "Buscas/buscaBinaria.h"
 
 int main () {
+    srand(time(NULL));
+
 #ifdef _WIN32
     system("chcp 65001");
 #endif
@@ -19,19 +22,25 @@ int main () {
     TAdm *adm_encontrado;
     TCandidato *cand_encontrado;
     TEleitor *eleitor_encontrado;
+    
+    int qnt_adm = 2;
+    int qnt_cand = 3;
+    int qnt_ele = 5;
 
-    if ((log = fopen("log.txt", "w")) == NULL) {
+    if ((log = fopen("Data/log.txt", "w")) == NULL) {
         printf("Erro ao abrir arquivo de log\n");
         exit(1);
     }
 
-    // --- BLOCO DO ADMINISTRADOR ---
+    // ADMINISTRADOR ---
     printf("\n---------- ADMINISTRADOR ----------\n");
-    if ((arq_adm = fopen("administradores.dat", "w+b")) == NULL) {
+    if ((arq_adm = fopen("Data/administradores.dat", "w+b")) == NULL) {
         printf("Erro ao criar o arquivo administradores.dat\n");
         exit(1);
     } else {
-        criarBaseAdministrador(arq_adm, 2);
+        criarBaseAdministrador(arq_adm, qnt_adm);
+        int part_adm = (qnt_adm + 1) / 2;
+        classificacaoInternaAdministrador(arq_adm, part_adm);
         imprimirBaseAdministrador(arq_adm);
 
         printf("\nBuscando Sequencial por adm codigo 2...\n");
@@ -45,12 +54,14 @@ int main () {
 
     // CANDIDATO
     printf("\n\n\n\n---------- CANDIDATO ----------\n");
-    if ((arq_cand = fopen("candidatos.dat", "w+b")) == NULL) {
+    if ((arq_cand = fopen("Data/candidatos.dat", "w+b")) == NULL) {
         printf("Erro ao criar o arquivo candidatos.dat\n");
         exit(1);
     } else {
-        criarBaseCandidato(arq_cand, 3);
+        criarBaseCandidato(arq_cand, qnt_cand);
         imprimirBaseCandidato(arq_cand);
+        int part_cand = (qnt_cand + 1) / 2;
+        classificacaoInternaCandidato(arq_cand, part_cand);
 
         printf("\nBuscando Sequencial por candidato codigo 3...\n");
         cand_encontrado = buscaSequencialCandidato(3, arq_cand, log);
@@ -63,13 +74,15 @@ int main () {
 
     // ELEITOR
     printf("\n\n\n---------- ELEITOR ----------\n");
-    if ((arq_eleitor = fopen("eleitores.dat", "w+b")) == NULL) {
+    if ((arq_eleitor = fopen("Data/eleitores.dat", "w+b")) == NULL) {
         printf("Erro ao criar o arquivo eleitores.dat\n");
         exit(1);
     } else {
-        criarBaseEleitor(arq_eleitor, 4);
+        criarBaseEleitor(arq_eleitor, qnt_ele);
         imprimirBaseEleitor(arq_eleitor);
-        
+        int part_ele = (qnt_ele + 1) / 2;
+        classificacaoInternaEleitor(arq_cand, part_ele);
+
         printf("\nBuscando Sequencial por eleitor codigo 1...\n");
         eleitor_encontrado = buscaSequencialEleitor(1, arq_eleitor, log);
         if(eleitor_encontrado != NULL){
